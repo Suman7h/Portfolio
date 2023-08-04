f3835d59a9571d824b66c0aaa2c0bdddb20bb282
Venkata Sumanth Peruri <sumanthperuri3@gmail.com>
2023-08-03 23:41:41 -0400

    Made changes in Experience

--- src/components/timelineElements.js
+++ src/components/timelineElements.js
@@ -4,7 +4,14 @@ let timelineElements = [
       title: "University Of Central Florida",
       location: "Masters In Computer Science - Orlando, FL",
       description:
-        `Learning Advance Concepts of Computer Science. Taking part in Hackthons and events to improve the knowledge and connections. Learning new concepts such as "System Desgin", "DSA". Making responsive webpages that involves learning and implementing "Talidwind CSS" and "Reactjs". Learning advance cocepts like "Computer Vision", "Neural Networks", "API", "Nodejs". Also getting certified in "Front and Back end libraries" from FreeCodeCamp.  `,
+        `<ul>
+          <li>• Developed a model to perform "Semantic Segmentation" using Unet++ and obtained "85%" accuracy.</li>
+          <li>• Developed a model to predict poential defaulter for the credit card, by improving the accuray by "8%" using XGBoost. </li>
+          <li>• Developing responsive webpages that involves learning and implementing of "FullStack" Concepts.</li> 
+          <li>• Learning new concepts such as "System Desgin", "DSA","Computer Vision", "Neural Networks".</li> 
+          <li>• Taking part in Hackthons and events to improve the knowledge and connections.</li>
+          <li>• Obtained certification in "Front and Back end libraries" from FreeCodeCamp.</li>
+          </ul>  `,
       date: "August 2022 - present",
       icon: "school",
     },
@@ -14,11 +21,12 @@ let timelineElements = [
       location: "Sify Technologies, India",
       description:
       `<ul>
-      <li>• Involvement in improving the client's security compliance from 80% to 88% by proactive virus and threat detection.</li>
+      <li>• Involvement in improving the client's security compliance from "80% to 88%" by proactive virus and threat detection.</li>
       <li>• Establishing security for new clients by adhering to the "HLD" document and taking all necessary security precautions to protect the client environment from the attacks.</li>
       <li>• Examining the incidents and offering a quick fix to prevent any impact on the environment.</li>
       <li>• Worked on "cloud" environment like "AWS", "Docker" and "VM Ware", where the servers are securely configured for the clients using "Firewalls", "Antivirus".</li>
       <li>• Worked as DBA intern where I performed backups, installations, upgrades, performance tweaking, and bug fixes.</li>
+      <li>• Obtained certification as "Certified Ethical Hacker" from Ec-Council.</li>
     </ul>`,
       date: "July 2020 - June 2022",
       icon: "work",
@@ -28,7 +36,12 @@ let timelineElements = [
       title: "Sastra University",
       location: "Bachelor's in Information and Communication Technology",
       description:
-        `Continuous learning of all computer science topics, such as data structures, algorithms, and programming languages like C, C++, Java, Python, and Web development. Taking part in and hosting events where extra-circular activities like Hackathons, Events, etc. are taking place as Head of ICTAS. Also gaining certification in "Machine Learning" from coursera.`,
+        `<ul>
+        <li>• Developing a framework to know best SmartPhone by evaluating reviews using concept like IVLPTS.</li>
+        <li>• learning computer science topics, such as Data Structures, algorithms, and programming languages like C, C++, Java, Python, and Web development.</li>
+        <li>• Taking part in and hosting events where extra-circular activities like Hackathons, Events, etc. are taking place as Head of ICTAS.</li>
+        <li>• Obtained certification in "Machine Learning" from coursera.</li>
+        </ul>`,
       date: "July 2016 - May 2020",
       icon: "school",
     },
